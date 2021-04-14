d499c339ebfcbb10d855bf285c4558fc80b3bdd5
Ethan Lee <hello@ethanl.ee>
2021-04-13 20:28:18 -0700

    Add Safari support

--- background.js
+++ background.js
@@ -1,22 +1,25 @@
 // Taken in large part from short-d/short-ext :)
 const prefix = "bp/";
 const searchProviders = [
-  "google.com/search?",
-  "bing.com/search?",
-  "duckduckgo.com/?",
-  "ecosia.org/search?"
+  "google.com/search",
+  "bing.com/search",
+  "duckduckgo.com/",
+  "ecosia.org/search",
 ];
 const filter = {
-  urls: [`*://${prefix}*`, ...searchProviders.map(url => `*://*.${url}*`)],
-  types: ["main_frame"]
+  urls: [`*://${prefix}*`, ...searchProviders.map((url) => `*://*.${url}*`)],
+  types: ["main_frame"],
 };
 
 function isFromAddressBar(url) {
-  return searchProviders.some(pattern => url.indexOf(pattern) !== -1);
+  return searchProviders.some((pattern) => url.indexOf(pattern) !== -1);
 }
 
+const userAgent = navigator.userAgent.toLowerCase();
+const isSafari = userAgent.includes("safari") && !userAgent.includes("chrome");
+
 chrome.webRequest.onBeforeRequest.addListener(
-  details => {
+  (details) => {
     let { url } = details;
 
     if (isFromAddressBar(url)) {
@@ -24,8 +27,20 @@ chrome.webRequest.onBeforeRequest.addListener(
     }
     if (url.includes(prefix)) {
       url = url.substring(url.lastIndexOf(prefix) + prefix.length).trim();
+      const redirectUrl = `https://go.calblueprint.org/?q=${
+        isSafari ? encodeURIComponent(url) : url
+      }`;
+
+      if (isSafari) {
+        chrome.tabs.getCurrent((tab) =>
+          chrome.tabs.update(tab.id, {
+            url: redirectUrl,
+          })
+        );
+      }
+
       return {
-        redirectUrl: `https://go.calblueprint.org/?q=${url}`
+        redirectUrl,
       };
     }
   },
---
+++ create-dmg.sh
@@ -0,0 +1,10 @@
+create-dmg --volname "bpslash for Safari" \
+  --window-pos 200 120 \
+  --window-size 800 440 \
+  --icon-size 100 \
+  --icon "bpslash.app" 200 190 \
+  --background "dmg-bg.png" \
+  --hide-extension "bpslash.app" \
+  --app-drop-link 600 185 \
+  "bpslash.dmg" \
+  "safari/dist"
